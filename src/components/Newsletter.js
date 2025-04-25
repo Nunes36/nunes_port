bd981730d97ba0ad71d0dd42aca802a601287068
Matheus Ferreira <theuswork@outlook.com>
2025-04-24 21:43:16 -0300

    pequena alteração

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -26,14 +26,14 @@ export const Newsletter = ({ onValidated, subscribe, status, message }) => {
         <Col lg={12}>
             <div className="newsletter-bx">
                 <Row>
-                    <Col lg={12} md={6} xl={5}>
+                    <Col lg={12} md={6} xl={5} className="display-flex">
                         <h3>Assine nossa Newsletter</h3>
                         {status === 'sending' && <Alert>Sending...</Alert>}
                         {status === 'error' && <Alert variant="danger">{message}</Alert>}
                         {status === 'success' && <Alert variant="success">{message}</Alert>}
                     </Col>
                     <Col md={6} xl={7}>
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmit} >
                             <div className="new-email-bx">
                                 <input value={email} type="email" onChange={(e) => 
                                     setEmail(e.target.value)} placeholder="Email Address"/>
