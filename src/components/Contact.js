b55f9b042e7511842eb2c640e72c07432bc8f835
Matheus Ferreira <theuswork@outlook.com>
2025-05-05 18:15:35 -0300

    adicionando patterns

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -63,11 +63,13 @@ export const Contact = () => {
                                 </Col>
                                 <Col size={12} sm={6} className="px-1">
                                 <input type="email" value={formDetails.email} placeholder="Email" 
-                                    onChange={(e) => onFormUpdate('email', e.target.value)} required/>
+                                    onChange={(e) => onFormUpdate('email', e.target.value)} required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                                    title="Digite um e-mail válido (ex: usuario@dominio.com)"/>
                                 </Col>
                                 <Col size={12} sm={6} className="px-1">
                                 <input type="tel" value={formDetails.phone} placeholder="Telefone" 
-                                    onChange={(e) => onFormUpdate('phone', e.target.value)} required/>
+                                    onChange={(e) => onFormUpdate('phone', e.target.value)} required pattern="^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$"
+                                    title="Digite um telefone válido, como (11) 91234-5678"/>
                                 </Col>
                                 <Col size={12} sm={6} className="px-1">
                                     <textarea rows="6" value={formDetails.message} placeholder="Mensagem"
