d4f33af9ea732c2de28eb561e1a2275802a21023
Matheus Ferreira <theuswork@outlook.com>
2025-05-05 17:31:00 -0300

    adicionando o required

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -55,23 +55,23 @@ export const Contact = () => {
                             <Row>
                                 <Col size={12} sm={6} className="px-1">
                                     <input type="text" value={formDetails.firstName} placeholder="Nome" 
-                                    onChange={(e) => onFormUpdate('firstName', e.target.value)}/>
+                                    onChange={(e) => onFormUpdate('firstName', e.target.value)} required/>
                                 </Col>
                                 <Col size={12} sm={6} className="px-1">
                                 <input type="text" value={formDetails.lastName} placeholder="Sobrenome" 
-                                    onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                                    onChange={(e) => onFormUpdate('lastName', e.target.value)} required/>
                                 </Col>
                                 <Col size={12} sm={6} className="px-1">
                                 <input type="email" value={formDetails.email} placeholder="Email" 
-                                    onChange={(e) => onFormUpdate('email', e.target.value)}/>
+                                    onChange={(e) => onFormUpdate('email', e.target.value)} required/>
                                 </Col>
                                 <Col size={12} sm={6} className="px-1">
                                 <input type="tel" value={formDetails.phone} placeholder="Telefone" 
-                                    onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                                    onChange={(e) => onFormUpdate('phone', e.target.value)} required/>
                                 </Col>
                                 <Col size={12} sm={6} className="px-1">
                                     <textarea rows="6" value={formDetails.message} placeholder="Mensagem"
-                                    onChange={(e) => onFormUpdate('message', e.target.value)}/>
+                                    onChange={(e) => onFormUpdate('message', e.target.value)} required/>
                                     <button type="submit"><span>{buttonText}</span></button>
                                 </Col>
                                 <Col>
